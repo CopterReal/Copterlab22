57426a032ff64551f4d5e8816114c2f241347df6
Copter <copter101x18@gmail.com>
2024-02-27 14:41:03 +0700

    completed

--- average.cpp
+++ average.cpp
@@ -1,41 +1,18 @@
 #include <iostream>
-#include <vector>
-#include <sstream>
-
 using namespace std;
 
-int calAverage(vector<int>& numbers) {
-    int sum = 0;
-    for (int num : numbers) {
-        sum += num;
-    }
-    return sum / numbers.size();
-}
-
-int main(int argc, char *argv[]) {
-    if (argc <= 1) {
-        cout << "Please input numbers to find average." << endl;
-    } else {
-        vector<int> numbers;
-        for (int i = 1; i < argc; ++i) {
-            stringstream ss(argv[i]);
-            int num;
-            if (ss >> num) {
-                numbers.push_back(num);
-            }
+int main(int argc, char *argv[])
+{
+    float sum = 0;
+    if (argc == 1)  cout << "Please input numbers to find average." << endl;
+    else
+    {
+        for (int i = 0; i < argc - 1; i++)
+        {
+            sum += atof(argv[i + 1]);
         }
-
-        if (!numbers.empty()) {
-            int average = calAverage(numbers);
-            cout << "---------------------------------" << endl;
-            cout << "Average of " << numbers.size() << " numbers = ";
-            cout << average << endl;
-            cout << "---------------------------------" << endl;
-        } else {
-            cout << "No valid numbers provided." << endl;
-        }
+        cout << "---------------------------------" << endl;
+        cout << "Average of " << argc - 1 << " numbers = " << sum  / double(argc - 1) << endl;
+        cout << "---------------------------------" << endl;
     }
-
-    return 0;
 }
-
